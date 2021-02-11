dce57a2330ddb19d9a520dcb87157cc4c529393d
vagrant <vagrant@vagrant.vm>
2021-02-11 21:58:08 +0000

    uploaded data

--- static/stocks.js
+++ static/stocks.js
@@ -8,31 +8,33 @@ function get_stock_info(evt){
     console.log(symbol);
 
 
-    $.get('/api/stock?symbol='+ symbol, updateinfo);
+    $.get('/api/stock?symbol='+ symbol, updateInfo);
 
 
 }
 function updateInfo(results){
-    // console.log(results);
-    if (results.status == "success"){
-        $('#Name').html(results.Name);    
-       $('#Symbol').html(results.Symbol);  
-       $('#Address').html(results.Address);
-       $('#FullTimeEmployees').html(results.FullTimeEmployees);
-       $('#AssetType').html(results.AssetType);
-       $('#DividendPerShare').html(results.DividendPerShare);
-       $('#DividendYield').html(results.DividendYield);
-       $('#DividendPerShare').html(results.DividendPerShare);
-       $('#PEGRatio').html(results.PEGRatio);
-       $('#PERatio').html(results.PERatio);
-       $('#52WeekHigh').html(results.Week52High);
-       $('#52WeekLow').html(results.Week52Low);
-       $('#EPS').html(results.EPS);
-      
+    $(".company_info").show();
+    $('#Name').html(results.Name);    
+    $('#Symbol').html(results.Symbol);  
+    $('#Address').html(results.Address);
+    $('#FullTimeEmployees').html(results.FullTimeEmployees);
+    $('#AssetType').html(results.AssetType);
+    $('#DividendPerShare').html(results.DividendPerShare);
+    $('#DividendYield').html(results.DividendYield);
+    $('#DividendPerShare').html(results.DividendPerShare);
+    $('#PEGRatio').html(results.PEGRatio);
+    $('#PERatio').html(results.PERatio);
+    $('#52WeekHigh').html(results.Week52High);
+    $('#52WeekLow').html(results.Week52Low);
+    $('#EPS').html(results.EPS);
+   
 
-    }
 }
 
+function favorite(like){
+    $('.empty-like-button').show();
 
 
+
+}
 $("#stock_select").on('submit', get_stock_info);
