16dafa419f63fe28d2abe2abc7364c51bbb344ab
vagrant <vagrant@vagrant.vm>
2021-02-17 22:05:59 +0000

    defenitions

--- seed_database.py
+++ seed_database.py
@@ -20,3 +20,14 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 
+
+
+save_stocks(all_stocks)
+all_stocks = get_all_stocks()
+db.session.add(stocks)
+db.session.commit()
+
+new_user = User(email='admin@website.com', password='admin')
+
+db.session.add(new_user)
+db.session.commit()
--- static/stocks.js
+++ static/stocks.js
@@ -57,35 +57,37 @@ function favorite(evt){
 
     $.post('/api/favorite', {"stock_id": stock_id}, favorite_table);
 
+};
 
+function wordDef(){
 
-}
-// var favoriteBox =document.getElementById("favorite-table");
-// var btn = document.getElementById("btn");
-// function renderHTML(favorites){
+    var messages = {
+        "EPS": "Earnings Per Share (EPS) - is defined as Net Income divided by the total number of outstanding shares. This measure tells you the accounting profit of the company that each share is entitled to. ",
+        "Dividend Yield": "The ratio of the company's annual dividend compared to its share price.",
+        "Dividend Per Share" : "The sum of declared dividends issued by a company for every ordinary share outstanding.",
+        "PERRatio": "The ratio for valuing a company that measures its current share price relative to its per-share earnings (EPS). The price-to-earnings ratio is also sometimes known as the price multiple or the earnings multiple.",
+        "PEGratio": "The 'PEG ratio' (price/earnings to growth ratio) is a valuation metric for determining the relative trade-off between the price of a stock, the earnings generated per share (EPS), and the company's expected growth. In general, the P/E ratio is higher for a company with a higher growth rate.",
+        "52WeekHigh": "The 52-week high/low is the highest and lowest price at which a security, such as a stock, has traded during the time period that equates to one year.",
+        "52WeekLow": "The 52-week high/low is the highest and lowest price at which a security, such as a stock, has traded during the time period that equates to one year."
+    };
 
-//     var favs = ($.get('/api/userfavorite'));
-// // }
+    $(".content").on("click", "span", function(e) {
+        e.stopPropagation();
+        var $this = $(this),
+        _text = $this.text();
 
-// function get_definition {
-//     let displayCompany = document.getElementById("display-Company");
-
-//     word.addEventListener('mouseover', (get_definition)=> word.innerText = '')
-
-//     word.addEventListener('mouseleave', (get_definition) => word.innerText = '')
-
-//   }
-
-// $(document).ready(get_stock_info(){
-//     $( "#definitions" ).hover(get_stock_info() {
-//            $('.word').word({
-//         show: true
-//     });
-//   });  
-// });
+        var dialogContent = messages[_text.toLowerCase()];
+        if (dialogContent && dialogContent.length > 0) {
+        $("#dialog").dialog({
+            "modal": true,
+            "title": _text
+        }).html(dialogContent);
+        }
+    });
+    };
 
 
 $("#stock_select").on('submit', get_stock_info);
 $("#not_favorite").on('click', favorite);
-$(document).ready(favorite_table())
-
+$("#dialog").on('click', wordDef());
+$(document).ready(favorite_table());
